982a9ca99e85f59f901585200c1994d83084b729
vaniv <athv82237@gmail.com>
2024-05-13 19:36:17 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import Appointments from "./components/Appointments";
+import Doctors from "./components/Doctors";
+import Patients from "./components/Patients";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const isLinkActive = (path) => window.location.pathname === path;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h1 style={{ color: "teal" }}>Hospital Managment System</h1>
+        <nav>
+          <div className="sidenav">
+            <a className={isLinkActive("/appointments") ? "active" : ""} href="/appointments">
+            Appointments
+            </a>
+            <a className={isLinkActive("/appointments") ? "active" : ""} href="/patients">
+            Patients
+            </a>
+            <a className={isLinkActive("/appointments") ? "active" : ""} href="/doctors">
+            Doctors
+            </a>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/" element={<Appointments />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/patients" element={<Patients />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
